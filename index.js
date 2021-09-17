63b547c776283701fb36120e28ee700f5ef6409c
Avinash Rajak <79404579+Avinash-Rajak@users.noreply.github.com>
2021-09-17 14:26:02 +0530

    counter app init

---
+++ index.js
@@ -0,0 +1,15 @@
+// document.getElementById("counter").innerText = 0
+
+let countEl = document.getElementById("counter");
+let count = 0
+function increment(){
+    count = count + 1
+    countEl.innerText = count
+    console.log (count)
+}
+
+function decrement(){
+    count = count - 1
+    countEl.innerText = count
+    console.log (count)
+}
