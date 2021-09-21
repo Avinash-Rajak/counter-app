ba1583535e210394664bfc4583179062afd9adc2
Avinash Rajak <79404579+Avinash-Rajak@users.noreply.github.com>
2021-09-21 20:11:52 +0530

    added previous entry function and reset feature

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 // document.getElementById("counter").innerText = 0
 
 let countEl = document.getElementById("counter");
-let count = 0
-function increment(){
-    count = count + 1
-    countEl.innerText = count
-    console.log (count)
+let count = 0;
+function increment() {
+  count = count + 1;
+  countEl.innerText = count;
+  console.log(count);
 }
 
-function decrement(){
-    count = count - 1
-    countEl.innerText = count
-    console.log (count)
+function decrement() {
+  count = count - 1;
+  countEl.innerText = count;
+  console.log(count);
 }
+let saveEl = document.getElementById("save-el");
 
-function save(){
-    console.log(count)
+function save() {
+  let entry =  count + " - "
+  saveEl.textContent += entry;
+
+  count = 0;
 }
